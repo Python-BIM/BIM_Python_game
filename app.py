2a0fd14bfdf31dbadee7fd06e8dde900f9eb5782
Nenad Ćirić <ciricnenad90@gmail.com>
2022-04-04 21:24:54 +0200

    Update app.py

---
+++ app.py
@@ -0,0 +1,28 @@
+from flask import Flask, jsonify
+from datetime import datetime
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello_world():
+    return "<h1><b>Dobro došli na Python obuku!</b></h1>"
+
+
+@app.route("/time")
+def show_time():
+    return jsonify(datetime.now())
+
+
+from flask import Response
+@app.route("/src")
+def show_source():
+    with open('app.py', 'r') as file:
+        resp = Response(file.read())
+        resp.headers['Content-type'] = 'text/plan'
+        return resp
+
+
+if __name__ == '__main__':
+    print('Web server is running ...',)
+    # app.secret_key=''
+    app.run(host='0.0.0.0', debug=True)
