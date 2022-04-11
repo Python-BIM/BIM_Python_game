9d898c7eb9d2729ea3c3235a614ceed5b309089c
zzexxyy <zzexxyy@gmail.com>
2022-04-11 18:25:12 +0200

    Povezo model sa app.py, takodje model moze da se koristi i kao nezavisni skript

--- model.py
+++ model.py
@@ -21,8 +21,11 @@ class Answers(db.Entity):
   the_value = Required(bool)
 
 
-db.bind(provider='sqlite', filename='database.sqlite', create_db=False)
-
-set_sql_debug(True)
-
-db.generate_mapping(create_tables=True)
+if __name__ == '__main__':
+  db.bind(provider='sqlite', filename='database.sqlite', create_db=True)
+  set_sql_debug(True)
+  db.generate_mapping(create_tables=True)
+else:
+  db.bind(provider='sqlite', filename='database.sqlite', create_db=False)
+  set_sql_debug(True)
+  db.generate_mapping(create_tables=True)
--- unos_pitanja_u_bazu.py
+++ unos_pitanja_u_bazu.py
@@ -6,14 +6,15 @@
 from model import *
 
 # Kroz SQL
-db.execute("INSERT INTO Questions VALUES(0, 'Šta vrać type(0)',10);")
-# INSERT INTO Answers VALUES(1, '0',0);
-# INSERT INTO Answers VALUES(2, 'flaot',0);
-# INSERT INTO Answers VALUES(3, 'false',0);
-# INSERT INTO Answers VALUES(4, 'int',0);
+# db.execute("INSERT INTO Questions VALUES(0, 'Šta vrać type(0)',10);")
+# db.execute("INSERT INTO Answers VALUES(1, '0',0);")
+# db.execute("INSERT INTO Answers VALUES(2, 'flaot',0);")
+# db.execute("INSERT INTO Answers VALUES(3, 'false',0);")
+# db.execute("INSERT INTO Answers VALUES(4, 'int',0);")
 
 
-# korak po korak
+# korak po korak interaktivno u python konzoli
+'''
 db.Users(email='mika@gmaio.com', nickname='Mika', password='sta', points=10)
 db.Users(email='joca@gmail.com', nickname='Joca', password='fajld', points=0)
 
@@ -22,8 +23,9 @@ db.Answers(answer='0', related_question=1, the_value=False)
 db.Answers(answer='float', related_question=1, the_value=False)
 db.Answers(answer='type', related_question=1, the_value=False)
 db.Answers(answer='int', related_question=1, the_value=True)
+'''
 
-
+# Ovaj je testiran
 with db_session:
     o1 = Answers(answer='5', related_question=5, the_value=False)
     o2 = Answers(answer='0b101', related_question=5, the_value=True)
