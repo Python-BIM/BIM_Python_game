900c4e28b1bb2028de95ab05021bdea298775ad2
zzexxyy <zzexxyy@gmail.com>
2022-04-18 11:01:37 +0200

    Popravljen CSS u game.html, dodata pitanja i timer

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, jsonify,Response, redirect, render_template, request, f
 from pony.flask import Pony
 from flask_login import LoginManager, UserMixin, login_required, login_user
 from datetime import datetime
+from random import randint
 from model import *
 
 app = Flask(__name__)
@@ -13,20 +14,20 @@ login_manager.login_view = '/'
 def load_user(user_id):
     return db.Users.get(id=user_id)
 
-# @app.route("/")
 @app.route("/game")
 @login_required
-# pogledati sta se desava kada odemo na /game
 def game():
     users = select(u for u in Users)
-    question = list(select(q for q in Questions))[0]
+    all_questions = list(select(q for q in Questions))
+    # izaberi slucajno pitanje
+    question = all_questions[ randint(0, len(all_questions)-1) ]
     data = {
         "users" : users,
         "question" : question
     }
-    return render_template("index.html", data=data)
+    return render_template("game.html", data=data)
+    # return render_template("index.html", data=data)
 
-# @app.route("/login")
 @app.route("/")
 def login():
     return render_template("login.html")
@@ -36,30 +37,22 @@ def logovanje ():
     username = request.form['user']
     password = request.form['password']
     user = db.Users.get(email=username)
-    sifra_iz_baze = user.password 
-   
-    if (sifra_iz_baze!=password):
+    if user:
+        sifra_iz_baze = user.password     
+        if (sifra_iz_baze == password):
+            print('logovanje OK')
+            login_user(user)
+            return redirect("/game")
+    else:    
         flash("Pogresan username ili password!")
         print("Pogresan username ili password!")
         return redirect("/")
-    else: 
-        print('+++ logovanje OK')
-        login_user(user)
-        return redirect("/game")
         
-
+    
 @app.route("/time")
 def show_time():
     return jsonify(datetime.now())
 
-
-
-@app.route("/src")
-def show_source():
-    with open('app.py', 'r') as file:
-        resp = Response(file.read())
-        resp.headers['Content-type'] = 'text/plan'
-        return resp
 
 
 if __name__ == '__main__':
--- model.py
+++ model.py
@@ -1,11 +1,11 @@
-from enum import unique
 from flask_login import UserMixin
 from pony.orm import *
+from csv import DictReader
 
 db = Database()
 
 class Users(db.Entity, UserMixin):
-  id = PrimaryKey(str)
+  id = PrimaryKey(int)
   email = Required(str, unique=True)
   nickname = Required(str, unique=True)
   password = Required(str)
@@ -20,14 +20,29 @@ class Questions(db.Entity):
 
 class Answers(db.Entity):
   answer = Required(str)
-  related_question = Required(Questions)
-  the_value = Required(bool)
+  related_question = Optional(Questions)  # Optional(Questions)
+  the_value = Required(int)
 
 
 if __name__ == '__main__':
   db.bind(provider='sqlite', filename='database.sqlite', create_db=True)
   set_sql_debug(True)
   db.generate_mapping(create_tables=True)
+
+  # import data from csv files into DB tables
+  with db_session:
+    with open('Users.txt') as f:    
+      [Users(**r)for r in DictReader(f)]
+
+    with open('Answers.txt') as f:    
+      [Answers(**r) for r in DictReader(f)]
+
+    with open('Questions.txt') as f:    
+      for i, q in enumerate(DictReader(f)):
+        Questions(**q).options.add( 
+          [Answers[4*i+1], Answers[4*i+2], Answers[4*i+3], Answers[4*i+4]]
+        )
+
 else:
   db.bind(provider='sqlite', filename='database.sqlite', create_db=False)
   set_sql_debug(False)
@@ -35,6 +50,27 @@ else:
 
 
 """
+q1 = Questions(name='John', **q)
+q1.cars 
+
+q = Questions(**r)
+q.tags.add([Answers['Party'], Answers['New Year']])
+
+with db_session:
+    o1 = Answers(answer='5', related_question=0, the_value=False)
+    o2 = Answers(answer='0b101', related_question=0, the_value=True)
+    o3 = Answers(answer='bin', related_question=0, the_value=False)
+    o4 = Answers(answer='Error', related_question=0, the_value=False)
+    Questions(question='Rezultat od bin(5) je?', options=[o1, o2, o3, o4], value_points=15)
+
+Questions(**r).tags.add([Answers['Party'], Answers['New Year']])
+
+q1 = Questions(**r)
+a1 = Answers(**a)
+a1.related_question = q1
+
+Answers(**a).related_question = Questions(**r)
+
 with db_session:
   with open('Questions.txt', 'r') as f:
     Lines = f.read().splitlines()
@@ -48,4 +84,6 @@ with db_session:
     Lines = f.read().splitlines()
 
     for i in range (len(Lines)):
-      Answers(question=Lines[i], value_points=i)"""
+      Answers(question=Lines[i], value_points=i)
+
+"""
